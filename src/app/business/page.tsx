1703fa12b7c0694a1cc34fceb3b9b219f71b9d78
chiomanwocha <nwochachioma1998@gmail.com>
2024-08-19 17:33:21 +0100

    feat: implemented seo

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -1,78 +1,30 @@
-"use client";
+import BusinessComp from "@/components/business";
+import { Metadata } from "next";
 
-import CustomButton from "@/components/common/CustomButton";
-import Shell from "@/components/common/Shell";
-import Hero from "@/components/sections/Hero";
-import handleScroll from "@/utils/handleScroll";
-import { sendGAEvent } from "@next/third-parties/google";
-import agrifyHero from "../../../public/images/business-hero.png";
-import agrifyHeroMobile from "../../../public/images/mobile-biz-hero.png";
-import Collaboration from "@/components/sections/Collaboration";
-import SolutionCard from "@/components/business/SolutionCard";
-import { Box } from "@chakra-ui/react";
-import CalculatorShot from "@/components/business/CalculatorShot";
-import useBusinessLogic from "@/utils/hooks/useBusinessLogic";
+export const metadata: Metadata = {
+  title: "Agrify - Partner with Us",
+  description:
+    "Offset your emissions and decarbonize your supply chain with Regenerative agriculture.",
+  openGraph: {
+    title: "Agrify - Partner with Us",
+    description:
+      "Offset your emissions and decarbonize your supply chain with Regenerative agriculture.",
+    images: [
+      {
+        url: "/images/seo-cover.jpeg",
+        alt: "Decarbonize with Regenerative Agriculture",
+        width: 1280,
+        height: 672,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    url: "https://agrifyafrica.xyz/business",
+  },
+};
 
 const Business = () => {
-  const { menu, solutions, router } = useBusinessLogic();
-
-  return (
-    <Shell
-      menu={menu}
-      btn={
-        <CustomButton
-          text="Invest in Nature"
-          variant="solid"
-          onClick={() => router.push("/invest-in-nature")}
-        />
-      }
-      backgroundColor="white"
-    >
-      <Hero
-        title="Offset your emissions and decarbonize your supply chain with Regenerative agriculture."
-        bgGradient="linear(#F8F5F4, white)"
-        btn={
-          <CustomButton
-            text="Learn More"
-            variant="outline"
-            onClick={() => {
-              handleScroll("solutions");
-              sendGAEvent("event", "business-learn-more", { value: "view" });
-            }}
-            padding={{ base: "12px 24px 12px 24px" }}
-          />
-        }
-        img={{
-          desktop: agrifyHero,
-          mobile: agrifyHeroMobile,
-          alt: "agrify business hero image",
-        }}
-      />
-      <Box px={{ base: 0, lg: 9 }}>
-        <Collaboration />
-      </Box>
-      <Box
-        display="flex"
-        flexDir="column"
-        alignItems={{ md: "center", lg: "initial" }}
-        gap={{ base: "40px", lg: "83px" }}
-        pt={{ base: "80px", lg: "144px" }}
-        px={{ base: 0, lg: 14 }}
-        id="solutions"
-      >
-        {solutions.map((item, index) => (
-          <SolutionCard
-            key={item.title}
-            index={index}
-            title={item.title}
-            text={item.text}
-            img={item.img}
-          />
-        ))}
-      </Box>
-      <CalculatorShot />
-    </Shell>
-  );
+  return <BusinessComp />;
 };
 
 export default Business;
--- src/app/invest-in-nature/page.tsx
+++ src/app/invest-in-nature/page.tsx
@@ -1,5 +1,28 @@
 import NatureForm from "@/components/invest-in-nature/NatureForm";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Agrify - Partner with Us",
+  description:
+    "Offset your emissions and decarbonize your supply chain with Regenerative agriculture.",
+  openGraph: {
+    title: "Agrify - Partner with Us",
+    description:
+      "Offset your emissions and decarbonize your supply chain with Regenerative agriculture.",
+    images: [
+      {
+        url: "/images/seo-cover.jpeg",
+        alt: "Decarbonize with Regenerative Agriculture",
+        width: 1280,
+        height: 672,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    url: "https://agrifyafrica.xyz/invest-in-nature",
+  },
+};
 
 const InvestInNature = () => {
   return (
